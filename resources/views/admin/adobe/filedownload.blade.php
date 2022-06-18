5b33bf8a169ef0a36effc96276842e8f92ebe1e3
natasari <penitasari93@gmail.com>
2022-06-18 11:11:44 +0700

    a href filedownload adobe

--- resources/views/admin/adobe/filedownload.blade.php
+++ resources/views/admin/adobe/filedownload.blade.php
@@ -159,7 +159,7 @@
                                         <a href="ringkasan">Ringkasan</a>
                                     </li>
                                     <li>
-                                        <a class="active" href="file_download">File Download</a>
+                                        <a class="active" href="filedownload">File Download</a>
                                     </li>
                                     <li>
                                         <a href="spesifikasi">Spesifikasi</a>
@@ -190,16 +190,16 @@
                                 <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-energy"></i><span class="sidebar-mini-hide">Mathematica</span></a>
                                 <ul>
                                 <li>
-                                        <a href="be_blocks.html">Ringkasan</a>
+                                        <a href="ringkasanMathematica">Ringkasan</a>
                                     </li>
                                     <li>
                                         <a href="filedownloadMathematica">File Download</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_api.html">Spesifikasi</a>
+                                        <a href="spesifikasiMathematica">Spesifikasi</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_api.html">Preview</a>
+                                        <a href="previewMathematica">Preview</a>
                                     </li>
                                     
                                 </ul>
@@ -208,16 +208,16 @@
                                 <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-layers"></i><span class="sidebar-mini-hide">LabVIEW</span></a>
                                 <ul>
                                 <li>
-                                        <a href="be_blocks.html">Ringkasan</a>
+                                        <a href="ringkasanLabview">Ringkasan</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_draggable.html">File Download</a>
+                                        <a href="filedownloadLabview">File Download</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_api.html">Spesifikasi</a>
+                                        <a href="spesifikasiLabview">Spesifikasi</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_api.html">Preview</a>
+                                        <a href="previewLabview">Preview</a>
                                     </li>
                                 </ul>
                             </li>
@@ -225,16 +225,16 @@
                                 <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-note"></i><span class="sidebar-mini-hide">Minitab</span></a>
                                 <ul>
                                 <li>
-                                        <a href="be_blocks.html">Ringkasan</a>
+                                        <a href="ringkasanMinitab">Ringkasan</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_draggable.html">File Download</a>
+                                        <a href="filedownloadMinitab">File Download</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_api.html">Spesifikasi</a>
+                                        <a href="spesifikasiMinitab">Spesifikasi</a>
                                     </li>
                                     <li>
-                                        <a href="be_blocks_api.html">Preview</a>
+                                        <a href="previewMinitab">Preview</a>
                                     </li>
                                     
                                 </ul>
