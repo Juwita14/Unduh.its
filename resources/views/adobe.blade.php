dc085bfc240b75108ac7c617af3a99a9ddaea6ac
natasari <penitasari93@gmail.com>
2022-06-27 11:01:08 +0700

    membenarkan adobe dan menambahkan file sql database

--- resources/views/adobe.blade.php
+++ resources/views/adobe.blade.php
@@ -71,7 +71,7 @@
                                             @foreach($ringkasan as $r)
                                                 <p> {!! html_entity_decode($r->ringkasan) !!}</p>
                                             @endforeach
-                                            <p>Informasi tentang Adobe, silakan kunjungi <a href="https://www.adobe.com/id_en/creativecloud.html?sdid=WKRCJ9VF&amp;mv=search&amp;ef_id=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE:G:s&amp;s_kwcid=AL!3085!3!447025602704!e!!g!!adobe!636853869!31669464044&amp;gclid=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE" target="_blank">Offical Adobe.</a></p>
+                                            <p>Informasi tentang Adobe Creative Cloud, silakan kunjungi <a href="https://www.adobe.com/id_en/creativecloud.html?sdid=WKRCJ9VF&amp;mv=search&amp;ef_id=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE:G:s&amp;s_kwcid=AL!3085!3!447025602704!e!!g!!adobe!636853869!31669464044&amp;gclid=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE" target="_blank">Offical Adobe.</a></p>
                                         </div>
                                     </div>
                                 </div>
@@ -80,7 +80,7 @@
                                 <!-- Photos -->
                                 <div class="tab-pane fade" id="search-filedownload" role="tabpanel">
                                     <div class="row items-push">
-                                        <div class="col-lg-12 border-b">
+                                        <!-- <div class="col-lg-12 border-b">
                                             <h4 class="h5 mb-5">
                                                 <a class="font-size-md  font-w700">File Panduan</a>
                                             </h4>
@@ -93,12 +93,12 @@
                                             </a>
                                             <br>
                                             @endforeach
-                                        </div>
+                                        </div> -->
                                         <div class="col-lg-12 border-b">
                                             <h4 class="h5 mb-5">
                                                 <a class="font-size-md  font-w700">File Installer</a>
                                             </h4>
-                                            <p>Informasi tentang file download Adobe, silakan kunjungi <a href="https://www.adobe.com/id_en/creativecloud.html?sdid=WKRCJ9VF&mv=search&ef_id=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE:G:s&s_kwcid=AL!3085!3!447025602704!e!!g!!adobe!636853869!31669464044&gclid=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE" target="_blank">Offical Adobe.</a></p>
+                                            <p>Informasi tentang file download Adobe Creative Cloud, silakan kunjungi <a href="https://www.adobe.com/id_en/creativecloud.html?sdid=WKRCJ9VF&mv=search&ef_id=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE:G:s&s_kwcid=AL!3085!3!447025602704!e!!g!!adobe!636853869!31669464044&gclid=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE" target="_blank">Offical Adobe.</a></p>
                                         </div>
                                     </div>
                                 </div>
@@ -107,8 +107,22 @@
                                 <!-- Spesifikasi -->
                                 <div class="tab-pane fade " id="search-spesifikasi" role="tabpanel">
                                     <div class="row items-push">
+                                        <!-- <div class="col-lg-12 border-b">
+                                            <h4 class="h5 mb-5">
+                                                <a class="font-size-md  font-w700">Persyaratan Sistem</a>
+                                            </h4>
+                                            @foreach($persyaratan_sistem as $p)
+                                                <p> {!! html_entity_decode($p->persyaratan_sistem) !!}</p>
+                                            @endforeach
+                                        </div> -->
                                         <div class="col-lg-12 border-b">
-                                            <p>Informasi tentang fitur dan spesifikasi produk Adobe Creative Cloud , silakan kunjungi <a href="https://www.adobe.com/id_en/creativecloud.html?sdid=WKRCJ9VF&mv=search&ef_id=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE:G:s&s_kwcid=AL!3085!3!447025602704!e!!g!!adobe!636853869!31669464044&gclid=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE" target="_blank">Offical Adobe.</a></p>
+                                            <!-- <h4 class="h5 mb-5">
+                                                <a class="font-size-md  font-w700">Fitur</a>
+                                            </h4>
+                                                @foreach($fitur as $f)
+                                                <p> {!! html_entity_decode($f->fitur) !!}</p>
+                                                @endforeach -->
+                                                <p>Informasi tentang fitur dan spesifikasi perangkat lunak Adobe Creative Cloud , silakan kunjungi <a href="https://www.adobe.com/id_en/creativecloud.html?sdid=WKRCJ9VF&mv=search&ef_id=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE:G:s&s_kwcid=AL!3085!3!447025602704!e!!g!!adobe!636853869!31669464044&gclid=CjwKCAjwtcCVBhA0EiwAT1fY74TX1e_0hDiKnJ73yDTW4ZaNo6t-v0_kOgK5KKx6GKw_y6h8H2G0ABoCthcQAvD_BwE" target="_blank">Offical Adobe.</a></p>
                                         </div>
                                         
                                     </div>
