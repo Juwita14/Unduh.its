8beecd57a616ab388d1f2b3465edee73d014c632
Juwita14 <jujuwita001@gmail.com>
2022-07-20 20:34:20 +0700

    print all session

--- src/resources/views/labview.blade.php
+++ src/resources/views/labview.blade.php
@@ -85,7 +85,7 @@
                                                 <a class="font-size-md  font-w700">File Panduan</a>
                                             </h4>
                                             @foreach($file_panduan as $pd)
-                                                @if(session()->get('id_token') == null)
+                                                @if(session()->get('id_token') != null)
                                                 <a href="/downloadPanduan/{{$pd->id}}">
                                                     <i class="fa fa-file-pdf-o" style="color:red"></i>
                                                     <div style="display:inline-block;" class="text">
