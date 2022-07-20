15cf0fb0fdb7f28e218a60935201bb488a2e83f7
Juwita14 <jujuwita001@gmail.com>
2022-07-20 20:21:45 +0700

    print all session

--- src/routes/web.php
+++ src/routes/web.php
@@ -258,7 +258,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::group(['middleware' => ['cek_login:user']], function () {
         Route::resource('produk', UserController::class);
-        Route::get('/downloadPanduan/{id}', [UserController::class, 'getDownloadPanduan']);
+        // Route::get('/downloadPanduan/{id}', [UserController::class, 'getDownloadPanduan']);
         Route::get('/downloadInstaller/{id}/{route}', [UserController::class, 'getDownloadInstaller']);
 
     });
