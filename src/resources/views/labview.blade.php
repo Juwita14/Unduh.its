60eb0a669da6b1d0cd05639443a55322a17cd779
Juwita14 <jujuwita001@gmail.com>
2022-07-20 16:12:56 +0700

    edit labview

--- src/resources/views/labview.blade.php
+++ src/resources/views/labview.blade.php
@@ -125,8 +125,7 @@
                                                     <td>{{$fi->size}} GB</td>
                                                         <td class="text-center">
                                                             <div class="btn-group">
-                                                                session()->has('login_session') == true
-                                                                @if(auth()->user()->name != NULL)
+                                                                @if(session()->has('login_session') == true)
                                                                 <a href="/downloadInstaller/{{$fi->id}}/{{ Request::path()}}">
                                                                     <button style="color:#3F9CE8" type="button" class="btn btn-sm btn-secondary" data-toggle="tooltip" title="Download">
                                                                         <i class="fa fa-download"></i>
@@ -136,6 +135,7 @@
                                                                     <button style="color:#3F9CE8" type="button" class="btn btn-sm btn-secondary" data-toggle="tooltip" title="Download">
                                                                         <i class="fa fa-download"></i>
                                                                     </button>
+                                                                </a>
                                                                 @endif
 
                                                             </div>
