485622727035824b968c32f3ecda946a4a933ce9
Juwita14 <jujuwita001@gmail.com>
2022-07-20 17:57:51 +0700

    edit labview

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -88,7 +88,7 @@ class LoginController extends Controller
                     session(['login_session' => 'user']);
                     return redirect()->intended('/');
                 }
-                return redirect()->intended('/');
+                return redirect('auth');
             }
             catch (OpenIDConnectClientException $e) {
                 Auth::logout();
