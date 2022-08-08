3918784e8f5ec62bedb6e00a55aefc40a8f7b7b0
Juwita14 <jujuwita001@gmail.com>
2022-08-08 20:35:18 +0700

    edit create spesifikasi fitur microsoft

--- src/resources/views/admin/microsoft/createFiturMicrosoft.blade.php
+++ src/resources/views/admin/microsoft/createFiturMicrosoft.blade.php
@@ -6,21 +6,20 @@
         <!-- Hover Table -->
         <div class="block">
             <div class="block-header block-header-default">
-                <h3 class="block-title">Edit Fitur</h3>
+                <h3 class="block-title">Tambah</h3>
             </div>
             <div class="block-content">
-                <form action="/admin/microsoft/spesifikasiMicrosoft/storeMicrosoft}" method="POST" >
-                    @method('PUT')
-                    @csrf
+            <form action="/admin/microsoft/spesifikasiMicrosoft/storeMicrosoft" method="POST" >
+                @csrf
                     <div class="form-group row">
                         <label class="col-12">Fitur</label>
                         <div class="col-12">
-                            <textarea id="js-ckeditor" name="fitur">{{$fitur->fitur}}</textarea>
+                            <textarea id="js-ckeditor" name="fitur"></textarea>
                         </div>
                     </div>
                     <div class="form-group row">
                         <div class="col-12">
-                            <button type="submit" name=submit value="Update" class="btn btn-alt-primary">Submit</button>
+                            <button type="submit" name=submit value="Save" class="btn btn-alt-primary">Submit</button>
                         </div>
                     </div>
                 </form>
@@ -28,6 +27,6 @@
         </div>
             <!-- END Hover Table -->
     </div>
-    <!-- END Page Content -->
+<!-- END Page Content -->
 
 @endsection
